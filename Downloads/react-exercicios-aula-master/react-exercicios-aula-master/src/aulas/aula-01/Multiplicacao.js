57aa75abaf383f7f1b857b8b6f6487ea718be388
Luís Maia <142423930+lulmaia@users.noreply.github.com>
2025-09-05 18:00:09 -0300

    Delete Downloads directory

--- Downloads/react-exercicios-aula-master/react-exercicios-aula-master/src/aulas/aula-01/Multiplicacao.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-function Multiplicacao({ num1, num2 }) {
-    const resultado = num1 * num2;
-    return <h1>O resultado de {num1} * {num2} é igual a {resultado}</h1>;
-}
-
-export default Multiplicacao;
